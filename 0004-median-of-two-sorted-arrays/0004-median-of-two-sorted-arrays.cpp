6e10a6dee2e844e5ba96073007e7bd2ad89edccc
curiousgaurav <161939641+curiousgaurav@users.noreply.github.com>
2026-09-13 14:31:06 +0530

    Time: 0 ms (100%), Space: 95.7 MB (33.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,29 +1,41 @@
-// Brute Force:
-               // 1.Merge Both Array
-              // 2.Sort them
-             // 3.Find Median
-            // TIME COMPLEXITY: O(n)+O(nlogn)+O(n)
-            // SPACE COMPLEXITY: O(1)
- 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       // Initialization some neccessary variables
-        vector<int>v;
+        int n=nums1.size();
+        int m=nums2.size();
+        int i=0;
+        int j=0;
+        int k=0;
+        vector<int>arr(n+m);
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                arr[k]=nums1[i];
+                i++;
+                k++;
+
+            }else{
+                arr[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<n){
+            arr[k]=nums1[i];
+            k++;
+            i++;
+        }
+        while(j<m){
+            arr[k]=nums2[j];
+            k++;
+            j++;
+        }
+
+        int total=m+n;
+        if(total%2==0){
+            return (arr[total/2]+ arr[total/2-1])/2.0;
+        }else{
+            return arr[total/2];
+        }
         
-        // store the array in the new array
-        for(auto num:nums1)   // O(n1)
-            v.push_back(num);
-        
-        for(auto num:nums2)  // O(n2)
-            v.push_back(num);
-        
-        // Sort the array to find the median
-        sort(v.begin(),v.end());  // O(nlogn)
-        
-        // Find the median and Return it
-        int n=v.size();  // O(n)
-        
-        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
     }
 };
